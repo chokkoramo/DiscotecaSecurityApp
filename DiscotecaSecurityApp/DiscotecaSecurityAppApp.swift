a7c74dbaa4203afd4431800c99399db589805a9a
Juan Camilo Perdomo Vasquez <juan.perdomo323@pascualbravo.edu.co>
2025-10-07 15:59:34 -0500

    Initial Commit

---
+++ DiscotecaSecurityApp/DiscotecaSecurityAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiscotecaSecurityAppApp.swift
+//  DiscotecaSecurityApp
+//
+//  Created by Juan Camilo on 7/10/25.
+//
+
+import SwiftUI
+
+@main
+struct DiscotecaSecurityAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
